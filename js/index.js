9260f00ad7c43d157d9a2adfb717dd687cc2bb59
Maks-hub256 <61085030+Maks-hub256@users.noreply.github.com>
2021-03-20 07:56:23 +0200

    Completed

--- js/index.js
+++ js/index.js
@@ -51,25 +51,42 @@ class Page {
         const reposResp = await fetch(`https://api.github.com/users/${user.login.toLowerCase()}/repos?client_id=537eb1ccd6cf3d86e0dd&secret_id=ed69e0ea1b0c18f1fff0f079bff42f0f72fbea09&per_page=100`);
         const repos = await reposResp.json();
 
+        if(repos.length > 0) {
+            _(".main__user").innerHTML = `
+            <button id="back"><i class="fas fa-arrow-left"></i></button>
+            <div class="head">
+                <div class="head__img-wrapper">
+                    <img class="head__img" src="${user.avatar_url}" alt="">
+                    <p class="head__name">${user.login}</p>
+                    <button class="head__button" onclick="document.location.href='${user.html_url}'">Go to Github</button>
+                </div>
+                <div class="head__status">
+                    <ul>
+                        <li><strong>Type:</strong> ${user.type}</li>
+                        <li><strong>Repos:</strong> ${repos.length}</li>
+                    </ul>
+                </div>
+            </div>
+            <h2 class="repo-title">Repos</h2>
+            <div class="repos"></div>`
+        } else {
+            _(".main__user").innerHTML = `
+            <button id="back"><i class="fas fa-arrow-left"></i></button>
+            <div class="head">
+                <div class="head__img-wrapper">
+                    <img class="head__img" src="${user.avatar_url}" alt="">
+                    <p class="head__name">${user.login}</p>
+                    <button class="head__button" onclick="document.location.href='${user.html_url}'">Go to Github</button>
+                </div>
+                <div class="head__status">
+                    <ul>
+                        <li><strong>Type:</strong> ${user.type}</li>
+                        <li><strong>Repos:</strong> ${repos.length}</li>
+                    </ul>
+                </div>
+            </div>`
+        }
 
-        _(".main__user").innerHTML = `
-        <button id="back"><i class="fas fa-arrow-left"></i></button>
-        <div class="head">
-            <div class="head__img-wrapper">
-                <img class="head__img" src="${user.avatar_url}" alt="">
-                <p class="head__name">${user.login}</p>
-                <button class="head__button" onclick="document.location.href='${user.html_url}'">Go to Github</button>
-            </div>
-            <div class="head__status">
-                <ul>
-                    <li><strong>Type:</strong> ${user.type}</li>
-                    <li><strong>Repos:</strong> ${repos.length}</li>
-                </ul>
-            </div>
-        </div>
-        <h2 class="repo-title">Repos</h2>
-        <div class="repos"></div>
-        `
 
         _(".main__user").classList.add("show")
         _('.main__search').classList.add("hide")
